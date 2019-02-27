6c4be7636817677318d81c2933d49a04148244b9
Gema Gina <47233585+gigitb@users.noreply.github.com>
2019-02-27 11:12:01 -0600

    Add files via upload

---
+++ 27:02:2019/Ejercicio 6 .playground/Contents.swift
@@ -0,0 +1,109 @@
+//crear funciones
+func HolaMundo(){
+    print("El mundo no es plano")
+    print("afirman especialistas")
+}
+
+HolaMundo()
+
+/* //llamar funcion para acompletar canción
+func rowTheBoat() {
+    print("Rema, rema, rema tu bote")
+    print("Alegre por el río")
+}
+
+func merrilyDream() {
+    print("Rápido, rápido, rápido, rápido")
+    print("Rema en el barquito")
+}
+
+func breatheBetweenVerses() {
+    print("        ~        ")
+}
+*/
+
+
+
+
+rowTheBoat()
+merrilyDream()
+breatheBetweenVerses()
+rowTheBoat()
+
+func crocodileScream(){
+    print("el coco no, el coco no")
+    print("no, no, no el coco no, el coco no")
+}
+
+crocodileScream()
+
+// Escribe una función para una segunda estrofa alternativa de la canción usando la función del submarino que se ríe a continuación.
+
+func rowTheBoat() {
+    print("Rema, rema, rema tu bote")
+    print("Alegre por el río")
+}
+
+func merrilyDream() {
+    print("Rápido, rápido, rápido, rápido")
+    print("Rema en el barquito")
+}
+func laughingSubmarine() {
+    print("¡Ja, ja! Los engañé")
+    print("Soy un submarino")
+}
+
+// Escribe la función de la segundaestrofa más abajo
+
+func verseTwo(){
+    laughingSubmarine()
+    HolaCrayola()
+    
+}
+
+func HolaCrayola(){
+    print("amarillo chi cheñol")
+}
+
+
+func verseTwo(){
+    laughingSubmarine()
+    HolaCrayola()
+    
+}
+
+func verseOne() {
+    rowTheBoat()
+    merrilyDream()
+}
+
+verseOne()
+verseTwo
+
+//Cambia lineas de codigo
+
+func rowTheBoat() {
+    print("Mueve, mueve, mueve tu pedalMueve, mueve, mueve tu pedal")
+    print("Mueve, mueve, mueve tu pedal")
+}
+
+func verseOne() {
+    rowTheBoat()
+    merrilyDream()
+}
+
+func verseTwo() {
+    rowTheBoat()
+    crocodileScream()
+}
+
+func verseThree() {
+    rowTheBoat()
+    repetitiveTheme()
+}
+
+verseOne()
+breatheBetweenVerses()
+verseTwo()
+breatheBetweenVerses()
+verseThree()
